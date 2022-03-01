bb4ae89b83b9c2b422967cbfd402360d205c0918
unbtorsten <torsten.reuschel@unb.ca>
2022-03-01 19:57:32 -0400

    merge upstream updates

--- docs/collect_readmes.py
+++ docs/collect_readmes.py
@@ -4,6 +4,7 @@ from docutils.core import publish_doctree
 full_name_lut = {
     'a35t': 'Arty 35T',
     'a100t': 'Arty 100T',
+    'genesys2': 'Genesys 2',
     'nexys4ddr': 'Nexys 4 DDR',
     'basys3': 'Basys 3',
     'eos_s3': 'EOS S3',

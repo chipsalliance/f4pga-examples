622a40d1bdaf545cf70bae3847282aabf4a5ae24
Robert Szczepanski <rszczepanski@antmicro.com>
2022-11-03 14:39:14 +0100

    Add demo for qlf-k4n8

---
+++ qlf_k4n8/btn_counter/btn_counter.v
@@ -0,0 +1,13 @@
+module top (
+    input  wire       clk,
+    output wire [3:0] led
+);
+
+  reg [3:0] cnt;
+  initial cnt <= 0;
+
+  always @(posedge clk) cnt <= cnt + 1;
+
+  assign led = cnt;
+
+endmodule
